da259978e3eabbe5a03fce9e3e7e999c85c730c2
Aluno <michaelaraujo963@gmail.com>
2018-04-11 18:41:09 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ClienteService } from './services/cliente.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ErrorInterceptorProvider } from './services/interceptor-error';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroClientesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [ClienteService, ErrorInterceptorProvider],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
